65b6aa320ba4c1fc548122302b893f275b6093df
SWArch8 <archibongws@gmail.com>
2022-09-20 18:44:55 -0700

    deal

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * puts2 -> funtion prints out other char of a string
+ * @str: paramenter for string
+ * return: returns void
+*/
+
+void puts2(char *str)
+
+{
+
+	int len, i;
+
+
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
+
+	{
+
+		_putchar(str[i]);
+
+	}
+
+	  _putchar('\n');
+
+}
