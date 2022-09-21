9181ef453664024aa41498e13c2c9893116d495d
SWArch8 <archibongws@gmail.com>
2022-09-20 18:39:06 -0700

    gearedup

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * print_rev -> print a string in reverse order
+ * @s: the given string in reverse order
+ * return: void
+*/
+
+void print_rev(char *s)
+
+{
+
+	int n, i;
+
+
+
+	n = strlen(s);
+
+	for (i = n - 1; i >= 0; i--)
+
+	{
+
+		_putchar(s[i]);
+
+	}
+
+	_putchar('\n');
+
+}
