67f9038643a6a37147691bfd0db2e948871d8a65
SWArch8 <archibongws@gmail.com>
2022-09-20 18:41:54 -0700

    real

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * rev_string -> funtion that returns reverse strings
+ * @s: a charter of strings
+ * return: returns void
+*/
+
+void rev_string(char *s)
+
+{
+
+	char tmp;
+
+	int i, lenght1, lenght2;
+
+
+
+	lenght1 = 0;
+
+	lenght2 = 0;
+
+
+
+	while (s[lenght1] != '\0')
+
+		lenght1++;
+
+	lenght2 = lenght1 - 1;
+
+	for (i = 0; i < lenght1 / 2; i++)
+
+	{
+
+		tmp = s[i];
+
+		s[i] = s[lenght2];
+
+		s[lenght2] = tmp;
+
+		lenght2 -= 1;
+
+	}
+
+}
