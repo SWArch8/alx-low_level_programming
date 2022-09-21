c89716d976e1101522b52ed0e0762a7af23a2f12
SWArch8 <archibongws@gmail.com>
2022-09-20 18:47:40 -0700

    winner

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+#include <string.h>
+
+
+
+/**
+ * puts_half -> funtion prints half other char of a string
+ * @str: paramenter for string
+ * return: returns void
+*/
+
+void puts_half(char *str)
+
+{
+
+	int x, y, i;
+
+
+
+	x = strlen(str);
+
+	if (x % 2 == 1)
+
+		y = x / 2 + 1;
+
+	else
+
+		y = x / 2;
+
+	for (i = y; i < x; i++)
+
+		_putchar(str[i]);
+
+	_putchar('\n');
+
+}
