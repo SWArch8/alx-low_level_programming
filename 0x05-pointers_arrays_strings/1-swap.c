800b4bfc34024b52ed33776b45810d735981c11e
SWArch8 <archibongws@gmail.com>
2022-09-20 18:19:01 -0700

    hmmm

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * swap_int - given two integers swap the varable
+ * @a: parameter 1
+ * @b: parameter 2
+ * retun- always return 0
+*/
+
+void swap_int(int *a, int *b)
+
+{
+
+	int tmp;
+
+
+
+	tmp = *a;
+
+	*a = *b;
+
+	*b = tmp;
+
+}
